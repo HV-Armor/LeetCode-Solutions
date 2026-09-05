a5f3ca7bfaef04af547f7192f7b118fcb5e4b8d8
HV-Armor <harrshvardhan308174@gmail.com>
2026-09-06 01:08:39 +0530

    Time: 92 ms (80.47%), Space: 142.3 MB (5.41%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> window;
+        for (int i = 0; i < nums.size(); i++) {
+            if (window.count(nums[i]) && i - window[nums[i]] <= k) return true;
+            window[nums[i]] = i;
+        }
+        return false;
+    }
+};
