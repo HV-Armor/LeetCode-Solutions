ec6103217a0c34038d271a3b278e8b7f567bc058
HV-Armor <harrshvardhan308174@gmail.com>
2026-09-08 17:48:54 +0530

    Time: 0 ms (100%), Space: 9.3 MB (15.24%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void lpsfind(vector<int>&lps,string s){
+        int pre=0,suf=1;
+        while(suf<s.size()){
+            if(s[pre]==s[suf]){
+                lps[suf]=pre+1;
+                suf++,pre++;
+            } 
+            else{
+                if(pre==0){
+                    lps[suf]=0;
+                    suf++;
+                }
+                else{
+                    pre=lps[pre-1];
+                }
+            }
+        }
+    }
+    int strStr(string hay, string needle) {
+       vector<int> lps(needle.size(),0);
+       lpsfind(lps,needle);
+       int first =0, second=0;
+       while(second<needle.size()&& first<hay.size()){
+        if(needle[second]==hay[first]){
+            second++ , first++;
+        }
+        else{
+            if(second==0){
+                first++;
+            }
+            else
+            second=lps[second-1];
+        }
+       }
+       if(second==needle.size()){
+        return first-second;
+       }
+       else 
+       return -1;
+    }
+};
