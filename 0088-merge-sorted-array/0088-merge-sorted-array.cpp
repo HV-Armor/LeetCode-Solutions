bf0ac4d49c13c1c412dc966249f719bb0a16aa93
HV-Armor <harrshvardhan308174@gmail.com>
2026-09-09 00:07:59 +0530

    Time: 4 ms (1.12%), Space: 12.4 MB (85.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int low=m,high=0;
-        while(low<(m+n)){
+        while(low<nums1.size()){
             nums1[low]=nums2[high];
             low++;
             high++;
